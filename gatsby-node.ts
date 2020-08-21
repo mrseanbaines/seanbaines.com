92372a792846eade28094102b1032f8d1ecb265d
Sean Baines <mrseanbaines@gmail.com>
2020-08-21 14:12:45 +0100

    Set up absolute imports

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -3,6 +3,16 @@ import type Webpack from 'webpack'
 // FIXME: Make TypeScript recognize custom declaration file
 // @ts-ignore: No declaration file error
 import CleanTerminalPlugin from 'clean-terminal-webpack-plugin'
+import path from 'path'
+import fs from 'fs'
+
+const appDirectory = fs.realpathSync(process.cwd())
+const resolveApp = (relativePath: string) => path.resolve(appDirectory, relativePath)
+
+const paths = {
+  appSrc: resolveApp('src'),
+  appNodeModules: resolveApp('node_modules'),
+}
 
 export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({ stage, actions }) => {
   if (stage === 'develop') {
@@ -17,6 +27,9 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({ sta
             use: ['eslint-loader'],
           },
         ],
+      },
+      resolve: {
+        modules: ['node_modules', paths.appNodeModules, paths.appSrc],
       },
     }
 

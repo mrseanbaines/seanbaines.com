7351f3b59dee0c9c4f04ba93108603ad49de489a
Sean Baines <mrseanbaines@gmail.com>
2020-08-22 16:03:03 +0100

    Improve temporary SSR wrapPageElement typing

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import { WrapPageElementNodeArgs } from 'gatsby'
+import { GatsbySSR } from 'gatsby'
 
 import { WrapPageElement } from 'components/wrap-page-element'
 
-// TODO: Use GatsbySSR['wrapPageElement']?
-export const wrapPageElement = ({ element }: WrapPageElementNodeArgs) => <WrapPageElement>{element}</WrapPageElement>
+// TODO: Use GatsbySSR['wrapPageElement'] directly if possible
+type RequiredWrapPageElement = Required<GatsbySSR>['wrapPageElement']
+type Fn = (...args: Parameters<RequiredWrapPageElement>) => ReturnType<RequiredWrapPageElement>
+
+export const wrapPageElement: Fn = ({ element }) => <WrapPageElement>{element}</WrapPageElement>

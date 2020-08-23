78665257d2c4323b2160a4c6177314190c93e246
Sean Baines <mrseanbaines@gmail.com>
2020-08-23 18:43:06 +0100

    Add upper & align props to text

--- src/styles/text.ts
+++ src/styles/text.ts
@@ -12,12 +12,22 @@ type TextProps = ThemeProps &
     color?: keyof typeof theme.colors.text
     size?: number
     weight?: keyof typeof theme.fontWeights
+    upper?: boolean
+    align?: 'left' | 'center' | 'right'
   }
 
-const text = ({ theme, color, size, weight }: TextProps) => {
+const text = ({ theme, color, size, weight, upper, align }: TextProps) => {
   if (!!size && !theme.fontStyles[size]) {
     throw new Error('Invalid `size` prop')
   }
+
+  const upperStyles = css`
+    text-transform: uppercase;
+  `
+
+  const alignStyles = css`
+    text-align: ${align};
+  `
 
   return css`
     ${space}
@@ -27,12 +37,15 @@ const text = ({ theme, color, size, weight }: TextProps) => {
     font-size: ${(!!size && theme.fontStyles[size].size) || 'inherit'};
     font-weight: ${(!!weight && theme.fontWeights[weight]) || 'inherit'};
     line-height: ${(!!size && theme.fontStyles[size].lineHeight) || 'inherit'};
+
+    ${upper && upperStyles}
+    ${align && alignStyles}
   `
 }
 
-export const Text = styled.p<TextProps>(({ theme, color = 'default', size = 1, weight = 'medium' }) => {
+export const Text = styled.p<TextProps>(({ theme, color = 'default', size = 1, weight = 'medium', ...props }) => {
   return css`
-    ${text({ theme, color, size, weight })}
+    ${text({ theme, color, size, weight, ...props })}
   `
 })
 
